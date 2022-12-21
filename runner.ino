87abde53b5bb8d84c4355adfae6e2b7b25d60fd3
Dan Ortega <ortega.dan2010@gmail.com>
2022-12-20 22:33:40 -0500

    adding leds tester and motors move

--- runner.ino
+++ runner.ino
@@ -1,11 +1,23 @@
 #include "Arduino.h"
 
-void setup() {}
+void setup() {
+
+  // setting motor speed percentages
+  motorSpeed(M1, 25);
+  motorSpeed(M2, 25);
+}
 
 void loop() {
-  ledOn();
-  delay(300);
 
-  ledOff();
-  delay(300);
+  goForward(M1, M2);
+  delay(1000);
+
+  motorsOff(M1, M2);
+  delay(2000);
+
+  turnRight(M1, M2);
+  delay(1000);
+
+  motorsOff(M1, M2);
+  delay(2000);
 }
